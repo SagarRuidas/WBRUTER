f67af9f58b07ea2e50fdbffd5de526880b939245
wuseman <wuseman@nr1.nu>
2022-05-28 03:11:12 +0200

    s/MODEL/SHEALTHSAMSUNG/g

--- wbruter.sh
+++ wbruter.sh
@@ -88,7 +88,7 @@ add_to_conf() {
     DEVICE_VERSION="$(adb shell getprop ro.product.build.version.release)"
     DEVICE_ROOTED="$( adb shell hash su; [[ $? = "0" ]] && echo yes||echo no)"
     DEVICE_MODEL="$(adb shell getprop ro.product.model)"
-    DEVICE_MODEL="$(adb shell getprop init.svc.health-hal-2-1-samsung)"
+    DEVICE_HEALTHHAL="$(adb shell getprop init.svc.health-hal-2-1-samsung)"
 }
 
 show_help_android() {
